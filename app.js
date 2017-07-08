19a361da189c6d61587462b1a24f5c2958c77e12
Thadeu Luz <thadeuluz@gmail.com>
2017-07-08 02:54:58 -0300

    Create index.pug

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 })
 
 app.use((req, res, next) => {
