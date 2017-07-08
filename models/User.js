1dcc3451acf4b6f6c038f93fcde054f31e83e057
Thadeu Luz <thadeuluz@gmail.com>
2017-07-08 02:41:19 -0300

    Clean Slate

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-  email: { type: String, lowercase: true, required: true, index: true, unique: true },
-  name: { type: String, required: true },
-  password: String,
-});
-
-module.exports = mongoose.model('User', userSchema);
