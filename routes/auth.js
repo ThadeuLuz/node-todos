bdf482e7737d335e1c172b7da912f264ed897c05
Thadeu Luz <thadeuluz@gmail.com>
2017-07-08 03:50:16 -0300

    Signup backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const User = require('../models/User');
 
 const router = express.Router();
 
@@ -7,9 +9,28 @@ router.post('/signup', (req, res, next) => {
 
   if (!email || !name || !password) {
     req.flash('messages', 'All fields are required');
+    return res.redirect('/signup');
   }
 
-  return res.redirect('/signup');
+  User.findOne({ email }, (err, user) => {
+    if (err) return next(err);
+
+    if (user) {
+      req.flash('messages', 'There is a user with this email');
+      return res.redirect('/signup');
+    }
+
+    const newUser = new User({ email, name });
+    newUser.password = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+    newUser.save((saveErr, savedUser) => {
+      if (saveErr) return next(saveErr);
+
+      console.log('Usuário criado!', savedUser.id);
+
+      return res.redirect('/signup');
+    });
+  });
 });
 
 router.post('/login', (req, res, next) => {
