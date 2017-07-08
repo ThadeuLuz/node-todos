6be130dba0c325b4ff45bcc9d131df443f7fd53a
Thadeu Luz <thadeuluz@gmail.com>
2017-07-08 02:53:43 -0300

    Install pug, config

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
