6872403802dd226f37f4692968b7fbf9a4ca1772
Thadeu Luz <thadeuluz@gmail.com>
2017-07-08 02:48:56 -0300

    Tiny error handler, middleware concept

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.send(err.message);
+});
+
 app.listen(8080, () => {
   console.log(`Example app listening on port 8080!`);
 });
