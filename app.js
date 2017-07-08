46f371b3c62d143cc8bba329c2384a353e48bf6a
Thadeu Luz <thadeuluz@gmail.com>
2017-07-08 02:59:52 -0300

    Error page

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.send(err.message);
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err,
+  });
 });
 
 app.listen(8080, () => {
